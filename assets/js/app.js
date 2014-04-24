3c08211d460d3ca6a38e1f6d2129a0fd88e9f5ed
californiavol <charlesbrownroberts@gmail.com>
2014-04-23 19:34:55 -0700

    added app.js

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+
+var doughnutData = [
+									{
+										value: 70,
+										color:"#1abc9c"
+									},
+									{
+										value : 30,
+										color : "#ecf0f1"
+									}
+								];
+								var myDoughnut = new Chart(document.getElementById("javascript").getContext("2d")).Doughnut(doughnutData);
